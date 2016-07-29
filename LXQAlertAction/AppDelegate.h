ccedbe0fc37af0cecb5b3a186d92c90c35763242
LiuXinQian <1613565807@qq.com>
2016-07-29 09:01:50 +0800

    Initial Commit

---
+++ LXQAlertAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXQAlertAction
+//
+//  Created by 用户 on 16/7/29.
+//  Copyright © 2016年 XinQianLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXQAlertAction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXQAlertAction
+//
+//  Created by 用户 on 16/7/29.
+//  Copyright © 2016年 XinQianLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
